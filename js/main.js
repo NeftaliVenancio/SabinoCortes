80778b4945139996d3550d1f030c20614240a7ce
Neftali Venancio <42760809+NeftaliVenancio@users.noreply.github.com>
2025-04-25 17:55:49 -0600

    pagina de prueba
    se hacen modificaciones para la pagina de produccion en campaña

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Scroll suave personalizado (por compatibilidad más amplia)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+  
