0babd4356448e4ffa12a9cd13943339e18b410ab
Neftali Venancio <42760809+NeftaliVenancio@users.noreply.github.com>
2025-04-29 00:29:37 -0600

    final
    sitio terminado

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ window.addEventListener('load', () => {
     preloader.style.display = 'none';
   });
 });
+
+
+//Formulario
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Previene el envío del formulario por defecto
+
+  // Captura el nombre ingresado
+  var userName = document.getElementById("nombre").value;
+
+  // Oculta el formulario y muestra el mensaje de agradecimiento
+  document.getElementById("formulario-container").style.display = "none";
+  document.getElementById("thanks-message").style.display = "block";
+
+  // Actualiza el nombre en el mensaje de agradecimiento
+  document.getElementById("user-name").textContent = userName;
+});
